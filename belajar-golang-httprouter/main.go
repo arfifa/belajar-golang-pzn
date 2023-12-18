7189867f4ba462336396afd72e5ff9195fb8bff7
arfifa <arfifarahman509@gmail.com>
2023-12-18 17:03:18 +0700

    I have studied the httprouter library

---
+++ belajar-golang-httprouter/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Hello HttpRouter")
+	})
+	
+	server := http.Server{
+		Handler: router,
+		Addr: "localhost:8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
